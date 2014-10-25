89adfb5d627dcadbb14c6b6c0c8bbe5a3591eb44
jonas747 <jonasr747@gmail.com>
2014-10-25 18:44:48 +0200

    Added world package Moving away from storing the world in chunks into storing it in layers instead So insetad of filtering through chunks on every get chunk request we can just send them the whole layer since its fairly small

--- db/game.go
+++ db/game.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"fmt"
 	ferr "github.com/jonas747/fortia/error"
+	"strconv"
 )
 
 type GameDB struct {
@@ -10,12 +12,55 @@ type GameDB struct {
 
 // Returns the specified user's info
 func (g *GameDB) GetUserInfo(user string) (map[string]string, ferr.FortiaError) {
-	// TODO
-	return make(map[string]string), nil
+	return g.GetHash("u:" + user)
 }
 
 // Sets the specified users info fields from info map to whatever is in the info map
 func (g *GameDB) SetUserInfo(user string, info map[string]interface{}) ferr.FortiaError {
-	// TODO
-	return nil
+	return g.SetHash("u:"+user, info)
+}
+
+/*
+ - c:{xpos}:{ypos}:{zpos}
+	json with info
+ }
+*/
+func (g *GameDB) SetLayer(x, y, z int, layer []byte) ferr.FortiaError {
+	_, err := g.Cmd("SET", fmt.Sprintf("l:%d:%d:%d", x, y, z), layer)
+	return err
+}
+
+func (g *GameDB) GetLayer(x, y, z int) ([]byte, ferr.FortiaError) {
+	reply, err := g.Cmd("GET", fmt.Sprintf("l:%d:%d:%d", x, y, z))
+	if err != nil {
+		return []byte{}, err
+	}
+	out, nErr := reply.Bytes()
+	if nErr != nil {
+		return []byte{}, ferr.Wrap(nErr, "")
+	}
+	return out, nil
+}
+
+// // Returns a list of all chunks
+// func (g *GameDB) GetChunkList() ([]string, ferr.FortiaError) {
+// 	empty := []string{}
+// 	reply, err := g.Cmd("KEYS", "c*")
+// 	if err != nil {
+// 		return empty, err
+// 	}
+
+// 	list, nerr := reply.List()
+// 	if nerr != nil {
+// 		return empty, ferr.Wrap(nerr, "")
+// 	}
+// 	return list, nil
+// }
+
+func (g *GameDB) GetEntity(id int) (map[string]string, ferr.FortiaError) {
+	return g.GetHash("e:" + strconv.Itoa(id))
+}
+
+func (g *GameDB) SetEntity(id int, info map[string]interface{}) ferr.FortiaError {
+	return g.SetHash("e:"+strconv.Itoa(id), info)
 }
---
+++ world/world.go
@@ -0,0 +1,138 @@
+// Package world contains world realted stuff
+// TODO: Check if layer is air and mark it if so
+package world
+
+import (
+	"encoding/json"
+	"github.com/jonas747/fortia/db"
+	ferr "github.com/jonas747/fortia/error"
+	"github.com/jonas747/fortia/log"
+	"github.com/jonas747/fortia/vec"
+)
+
+type BlockFlag byte
+
+const (
+	BlockConnectedGround BlockFlag = 1 << iota
+	BlockOccupiedFull
+	BlockOccupiedHalf
+)
+
+type World struct {
+	Logger      *log.LogClient
+	Db          *db.GameDB
+	LayerSize   int
+	LayerHeight int // How many layers high the world is
+	WorldGen    WorldGenerator
+}
+
+func (w *World) LayerToWorldPos(layePos vec.Vec3I) vec.Vec3I {
+	lw := layePos.Clone()
+	lw.Multiply(vec.Vec3I{X: w.LayerSize, Y: w.LayerSize})
+	return lw
+}
+
+func (w *World) GenLayer(pos vec.Vec3I) *Layer {
+	//chunkWorldPos := w.ChunkToWorldPos(pos)
+
+	layer := &Layer{
+		Position: pos,
+	}
+
+	id := 1
+	if layer.Position.Z > w.LayerHeight/2 {
+		id = 0
+		layer.IsAir = true
+	}
+
+	blocks := make([]*Block, w.LayerSize*w.LayerSize)
+
+	for x := 0; x < w.LayerSize; x++ {
+		for y := 0; y < w.LayerSize; y++ {
+			b := Block{
+				LocalPosition: vec.Vec2I{x, y},
+				Layer:         layer,
+				Id:            id,
+			}
+			blocks[w.CoordsToIndex(vec.Vec3I{x, y, 0})] = &b
+		}
+	}
+
+	layer.Blocks = blocks
+	layer.World = w
+	return layer
+}
+
+// index = size * x + y
+func (w *World) CoordsToIndex(pos vec.Vec3I) int {
+	return w.LayerSize*pos.X + pos.Y
+}
+
+// Return a blocks x and y from the index in the layer slice
+// x = index / size
+// y = index - (x * size)
+func (w *World) IndexToCoords(index int) vec.Vec3I {
+	x := index / w.LayerSize
+	y := index - (x * w.LayerSize)
+	return vec.Vec3I{x, y, 0}
+}
+
+func (w *World) SetLayer(layer *Layer) ferr.FortiaError {
+	raw, err := layer.Json()
+	if err != nil {
+		return err
+	}
+	return w.Db.SetLayer(layer.Position.X, layer.Position.Y, layer.Position.Z, raw)
+}
+
+func (w *World) GetLayer(pos vec.Vec3I) (*Layer, ferr.FortiaError) {
+	rawLayer, err := w.Db.GetLayer(pos.X, pos.Y, pos.Z)
+	if err != nil {
+		return nil, err
+	}
+
+	layer := &Layer{
+		Position: pos,
+	}
+
+	nErr := json.Unmarshal(rawLayer, layer)
+	if nErr != nil {
+		return nil, ferr.Wrap(nErr, "")
+	}
+
+	return layer, nil
+}
+
+type Layer struct {
+	Position vec.Vec3I
+	Flags    int
+	World    *World `json:"-"`
+	Blocks   []*Block
+	IsAir    bool // True if this layer is just air
+}
+
+func (l *Layer) Json() ([]byte, ferr.FortiaError) {
+	out, err := json.Marshal(l)
+	if err != nil {
+		return []byte{}, ferr.Wrap(err, "")
+	}
+	return out, nil
+}
+
+type Block struct {
+	LocalPosition vec.Vec2I `json:"-"`
+	Layer         *Layer    `json:"-"`
+	Entities      []int     `json:",omitempty"`
+	Id            int
+	Flags         byte                   `json:",omitempty"`
+	Properties    map[string]interface{} `json:",omitempty"`
+}
+
+type Entity interface {
+	GetPosition()
+	GetId() int
+}
+
+type WorldGenerator interface {
+	GenerateBLock(x, y, z int) int
+}
