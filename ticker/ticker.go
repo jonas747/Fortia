f04d2399c7abea34102260a2223b100301989f85
jonas747 <jonasr747@gmail.com>
2014-11-22 04:40:29 +0100

    Work on the ticker

--- ticker/ticker.go
+++ ticker/ticker.go
@@ -1,22 +1,24 @@
 package ticker
 
 import (
-	"github.com/jonas747/fortia/db"
+	"github.com/jonas747/fortia/authserver"
 	ferr "github.com/jonas747/fortia/error"
 	"github.com/jonas747/fortia/log"
-	"github.com/jonas747/fortia/vec"
 	"github.com/jonas747/fortia/world"
 	"time"
 )
 
 var (
 	logger    *log.LogClient
-	authDb    *db.AuthDB
-	gameDb    *db.GameDB
+	authDb    authserver.AuthDB
+	gameDb    world.GameDB
 	gameWorld *world.World
+	handlers  map[string]Handler
 )
 
-func Run(l *log.LogClient, adb *db.AuthDB, gdb *db.GameDB) ferr.FortiaError {
+type Handler func(*world.Action) ferr.FortiaError
+
+func Run(l *log.LogClient, adb authserver.AuthDB, gdb world.GameDB) ferr.FortiaError {
 	logger.Info("Running world ticker")
 	logger = l
 	authDb = adb
@@ -30,59 +32,56 @@ func Run(l *log.LogClient, adb *db.AuthDB, gdb *db.GameDB) ferr.FortiaError {
 	if err != nil {
 		return err
 	}
-
+	registerAllHandlers()
 	run()
 	return nil
 }
 
-func generate(numX, numY int) {
-	num := 0
-	for x := 0; x < numX; x++ {
-		for y := 0; y < numY; y++ {
-			for z := 0; z < 100; z++ {
-				num++
-				layer := gameWorld.GenLayer(vec.Vec3I{x, y, z})
-				err := gameWorld.SetLayer(layer)
-				if err != nil {
-					logger.Error(err)
-				}
-				logger.Info("Generated layer ", num, " out of ", numX*numY*100)
-			}
-		}
-	}
+func registerHandler(name string, handler Handler) {
+	handlers[name] = handler
+}
+
+func registerAllHandlers() {
+	//....
 }
 
 func run() {
 	ticker := time.NewTicker(time.Duration(10) * time.Second)
 	for {
+		<-ticker.C
 		logger.Debug("Ticking now...")
 		startedTick := time.Now()
-		// keys, err := gameDb.GetChunkList()
-		// if err != nil {
-		// 	logger.Error(err)
-		// 	continue
-		// }
-		// finChan := make(chan bool)
-		// for _, v := range keys {
-		// 	split := strings.Split(v, ":")
-		// 	x, _ := strconv.Atoi(split[1])
-		// 	y, _ := strconv.Atoi(split[1])
 
-		// 	chunk, err := gameWorld.GetChunk(vec.Vec3I{X: x, Y: y})
-		// 	if err != nil {
-		// 		logger.Error(err)
-		// 		continue
-		// 	}
-		// 	go tickChunk(chunk, finChan)
-		// 	<-finChan
-		// }
+		nTick, err := gameDb.IncrTick()
+		if err != nil {
+			logger.Error(err)
+			continue
+		}
 
-		taken := time.Since(startedTick)
-		logger.Infof("Took %s to tick", taken.String())
-		<-ticker.C
+		for {
+			action, err := gameDb.PopAction(nTick)
+			if err != nil {
+				if err.GetCode() == world.ErrCodeNotFound {
+					taken := time.Since(startedTick)
+					logger.Infof("Took %s to tick", taken.String())
+					continue
+				} else {
+					logger.Error(err)
+				}
+			}
+
+			err = processAction(action)
+			if err != nil {
+				logger.Error(err)
+			}
+		}
 	}
 }
 
-func tickLayer(layer *world.Layer, done chan bool) {
-	done <- true
+func processAction(action *world.Action) ferr.FortiaError {
+	handlerFunc, ok := handlers[action.Handler]
+	if !ok {
+		return ferr.New("Unknown handler in action \"", action.Name, "\", Handler: \"", action.Handler, "\"")
+	}
+	return handlerFunc(action)
 }
